47c08eb79a43e7a4a67bd20226d5d956ee573c1b
Ryan Schmukler <rschmukler@gmail.com>
2014-02-02 18:39:59 -0500

    Renamed test file

--- test/gulp-insert-test.js
+++ test/gulp-insert-test.js
@@ -44,3 +44,48 @@ describe('Prepend', function() {
     stream.end();
   });
 });
+
+describe('Wrap', function() {
+  it('prepends the first argument and appends the second argument', function(done) {
+    var stream = insert.wrap('Hello ', '!');
+
+    var fakeFile = new File({
+      cwd: __dirname,
+      base: __dirname + 'test',
+      path: __dirname + 'test/file.js',
+      contents: new Buffer('world')
+    });
+
+    stream.on('data', function(file) {
+      expect(file.contents.toString()).to.be('Hello world!');
+      done();
+    });
+
+    stream.write(fakeFile);
+    stream.end();
+  });
+});
+
+describe('Transform', function() {
+  it('applys the function to the string', function(done) {
+    var stream = insert.transform(function(data) {
+      return data.toUpperCase();
+    });
+
+    var fakeFile = new File({
+      cwd: __dirname,
+      base: __dirname + 'test',
+      path: __dirname + 'test/file.js',
+      contents: new Buffer('hello world')
+    });
+
+    stream.on('data', function(file) {
+      expect(file.contents.toString()).to.be('HELLO WORLD');
+      done();
+    });
+
+    stream.write(fakeFile);
+    stream.end();
+
+  });
+});
